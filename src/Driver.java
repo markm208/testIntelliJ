8103fe30246861512887d6b1924986cd3afa3882
Mark Mahoney <markm208@gmail.com>
2026-05-22 16:04:41 -0500

    One print only

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-
-        int x = 10;
-
-        if(x < 100) {
-            System.out.println("Less than 100");
-        }
+        //System.out.println("Hello World!!!");
+        //System.out.println("Hello World!!!");
     }
 }
